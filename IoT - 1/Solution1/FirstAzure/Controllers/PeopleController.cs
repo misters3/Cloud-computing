3b489f4d60759d991a893f717bf47ae6b2c82277
misters3 <59235330+misters3@users.noreply.github.com>
2021-05-04 23:36:21 +0200

    IoT - 1

---
+++ IoT - 1/Solution1/FirstAzure/Controllers/PeopleController.cs
@@ -0,0 +1,29 @@
+﻿using FirstAzure.Controllers.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FirstAzure.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PeopleController : ControllerBase
+    {
+
+        private readonly PeopleDb db;
+        public PeopleController(PeopleDb db)
+        {
+            this.db = db;
+        }
+
+        [HttpGet]
+        public IActionResult Get() 
+        {
+            var people = db.People.ToList();
+            return Ok(people);
+        }
+    }
+}
---
+++ IoT - 1/Solution1/FirstAzure/PeopleDb.cs
@@ -0,0 +1,19 @@
+﻿using FirstAzure.Controllers.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FirstAzure
+{
+    public class PeopleDb : DbContext
+    {
+        public PeopleDb(DbContextOptions<PeopleDb> options) : base(options) 
+        {
+
+        }
+
+        public DbSet<Person> People { get; set; }
+    }
+}
