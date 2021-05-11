4bad81221b40a18d55949a0556428d11a36a707b
misters3 <59235330+misters3@users.noreply.github.com>
2021-05-11 22:17:41 +0200

    dodane zadanie mobile app1

---
+++ MobileApp - 1/zadanie 2/Phoneword/Phoneword/MainActivity.cs
@@ -0,0 +1,43 @@
+﻿using Android.App;
+using Android.OS;
+using Android.Widget;
+using AndroidX.AppCompat.App;
+
+namespace Phoneword
+{
+    [Activity(Label = "@string/app_name", Theme = "@style/AppTheme", MainLauncher = true)]
+    public class MainActivity : AppCompatActivity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.activity_main);
+
+            // New code will go here
+
+            // Get our UI controls from the loaded layout
+            EditText phoneNumberText = FindViewById<EditText>(Resource.Id.PhoneNumberText);
+            TextView translatedPhoneWord = FindViewById<TextView>(Resource.Id.TranslatedPhoneword);
+            Button translateButton = FindViewById<Button>(Resource.Id.TranslateButton);
+
+            // Add code to translate number
+            translateButton.Click += (sender, e) =>
+            {
+                // Translate user's alphanumeric phone number to numeric
+                string translatedNumber = Core.PhonewordTranslator.ToNumber(phoneNumberText.Text);
+                if (string.IsNullOrWhiteSpace(translatedNumber))
+                {
+                    translatedPhoneWord.Text = string.Empty;
+                }
+                else
+                {
+                    translatedPhoneWord.Text = translatedNumber;
+                }
+            };
+
+
+        }
+    }
+}
